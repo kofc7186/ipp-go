141094577629164eddac00e8d1023da43edfb3c5
Bob Callaway <bcallaway@google.com>
2024-03-16 15:10:46 -0500

    add b64 detection, cmd line flag for IP
    Signed-off-by: Bob Callaway <bcallaway@google.com>

--- main.go
+++ main.go
@@ -3,17 +3,28 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/base64"
 	"io"
 	"log"
 	"log/slog"
+	"net"
+	"net/url"
 	"os"
 	"os/exec"
+
+	flag "github.com/spf13/pflag"
 
 	"cloud.google.com/go/pubsub"
 	"google.golang.org/api/option"
 )
 
-var IPs = []string{"192.168.2.246"} //, "192.168.2.145"}
+var ipFlag net.IP
+var printerURL url.URL
+
+func init() {
+	flag.IPVar(&ipFlag, "ip", net.ParseIP("192.168.2.246"), "ip address of printer")
+	flag.Parse()
+}
 
 func main() {
 	ctx := context.Background()
@@ -22,6 +33,10 @@ func main() {
 		log.Fatal(err)
 	}
 
+	printerURL.Scheme = "ipp"
+	printerURL.Host = ipFlag.String()
+
+	slog.Info("subscribing to print_queue", "printerIP", printerURL.String())
 	sub := client.Subscription("print_queue")
 	sub.ReceiveSettings.MaxOutstandingMessages = 1
 	if err = sub.Receive(ctx, ReceivePubsubMessage); err != nil {
@@ -30,7 +45,7 @@ func main() {
 }
 
 func ReceivePubsubMessage(ctx context.Context, m *pubsub.Message) {
-	slog.InfoContext(ctx, "received message", "attributes", m.Attributes)
+	slog.InfoContext(ctx, "received document to print", "attributes", m.Attributes)
 
 	tempFile, err := os.CreateTemp("", m.ID)
 	if err != nil {
@@ -38,9 +53,11 @@ func ReceivePubsubMessage(ctx context.Context, m *pubsub.Message) {
 		m.Nack()
 		return
 	}
-	dec := bytes.NewReader(m.Data)
-	//dec := base64.NewDecoder(base64.StdEncoding, bytes.NewReader(m.Data))
-	if _, err := io.Copy(tempFile, dec); err != nil {
+	pdfBytes := m.Data
+	if b64DecodedBytes, err := base64.StdEncoding.DecodeString(string(m.Data)); err == nil {
+		pdfBytes = b64DecodedBytes
+	}
+	if _, err := io.Copy(tempFile, bytes.NewReader(pdfBytes)); err != nil {
 		slog.Error("error writing PDF to temp file", "err", err)
 		m.Nack()
 		return
@@ -58,12 +75,12 @@ func ReceivePubsubMessage(ctx context.Context, m *pubsub.Message) {
 	defer os.Remove(tempFile.Name())
 
 	//cmd := exec.Command("/Users/bcallaway/git/kofc7186/ipp-go/cups/tools/ipptool-static", "-f", tempFile.Name(), "ipp://192.168.2.246", "./print-job-and-wait.test")
-	cmd := exec.Command("./cups/tools/ipptool-static", "-f", tempFile.Name(), "ipp://192.168.2.246", "./print-job-and-wait.test")
+	cmd := exec.Command("./cups/tools/ipptool-static", "-f", tempFile.Name(), printerURL.String(), "./print-job-and-wait.test")
 	if err := cmd.Run(); err != nil {
-		slog.ErrorContext(ctx, "error invoking ipptool", "err", err)
+		slog.ErrorContext(ctx, "error invoking ipptool", "err", err, "son", m.Attributes["son"])
 		m.Nack()
 		return
 	}
-	slog.Info("printed order successfully")
+	slog.Info("printed order successfully", "son", m.Attributes["son"])
 	m.Ack()
 }
